bdec75b86d3e167a44ed8021be687a3ff0fe0444
Pork-stuffing <132484167+Pork-stuffing@users.noreply.github.com>
2024-03-27 21:22:45 +0800

    Add files via upload
    简单累加

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int N = pow(2,19);
+const int N = pow(2,28);
 int a[N];
 int sum=0;
 void init()
@@ -12,18 +12,6 @@ void init()
     for(int i =0;i<N;i++)
     {
         a[i]=i;
-    }
-}
-int recursion(int n)
-{
-    if(n==1)return 0;
-
-    else{
-        for(int i=0;i<n/2;i++){
-            a[i]+=a[n-i-1];
-        }
-        n=n/2;
-        recursion(n);
     }
 }
 int main()
@@ -34,8 +22,8 @@ int main()
     QueryPerformanceFrequency(&freq);
     // start time
     QueryPerformanceCounter(&head);
-    for(int i =0;i<100;i++){
-    recursion(N);
+    for(int i=0;i<N;i++){
+        sum+=a[i];
     }
      QueryPerformanceCounter(&tail);
     cout << "time: " << (tail.QuadPart - head.QuadPart) * 1000.0 / freq.QuadPart
